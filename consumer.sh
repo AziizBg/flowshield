3878a36e1161435ab4a9b6066774a2fec86c4b22
AzizBenGhorbel <mohamedaziz.benghorbel@insat.ucar.tn>
2025-06-09 11:09:14 +0100

    untrack .env file and add .gitignore to exclude it from version control; update consumer.sh to remove environment variable exports for configuration settings.

--- consumer.sh
+++ consumer.sh
@@ -1,23 +1,7 @@
 #!/bin/bash
 
-# Set output mode to HBase REST API
-export OUTPUT_MODE=hbase_rest
 
-# Kafka configuration
-export KAFKA_BOOTSTRAP_SERVERS=localhost:9092
-export EARTHQUAKE_TOPIC=earthquakes
-export FIRE_TOPIC=fires
 
-# HBase configuration
-export HBASE_REST_URL=http://hadoop-master:8080
-
-# Alert configuration
-export SMTP_SERVER=smtp.gmail.com
-export SMTP_PORT=587
-export ALERT_EMAIL=flowshield77@gmail.com
-export ALERT_EMAIL_PASSWORD=12345678A@
-export ALERT_RECIPIENT=testing7102023@gmail.com
-export ALERT_WEBHOOK_URL=https://hooks.slack.com/services/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
 
 # Submit Spark job
 spark-submit \
