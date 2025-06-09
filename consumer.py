61ef1999237be947f3023b58af05de9f537168b7
AzizBenGhorbel <mohamedaziz.benghorbel@insat.ucar.tn>
2025-06-09 11:18:55 +0100

    Refactor aggregation calculations in DataProcessor class to use simplified column references
    - Updated the aggregation logic to utilize direct column names instead of the col() function for calculating average and maximum values of magnitude and fire radiative power (frp).
    - Enhanced code readability and consistency in the aggregation process, contributing to improved maintainability.

--- consumer.py
+++ consumer.py
@@ -804,8 +804,8 @@ class DataProcessor:
             .groupBy("window", "severity") \
             .agg(
                 count("*").alias("count"),
-                avg(col("magnitude")).alias("avg_magnitude"),
-                max(col("magnitude")).alias("max_magnitude")
+                avg("magnitude").alias("avg_magnitude"),
+                expr("max(magnitude)").alias("max_magnitude")
             ) \
             .select(
                 col("window.start").alias("window_start"),
@@ -827,8 +827,8 @@ class DataProcessor:
             .groupBy("window", "severity") \
             .agg(
                 count("*").alias("count"),
-                avg(col("frp")).alias("avg_frp"),
-                max(col("frp")).alias("max_frp")
+                avg("frp").alias("avg_frp"),
+                expr("max(frp)").alias("max_frp")
             ) \
             .select(
                 col("window.start").alias("window_start"),
