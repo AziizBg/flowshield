c34eed6b5b3435bde8f589c8f5e4d9b84f707ba6
AzizBenGhorbel <mohamedaziz.benghorbel@insat.ucar.tn>
2025-06-09 10:43:59 +0100

    Add alert configuration to consumer.sh
    - Introduced environment variables for SMTP server settings, alert email credentials, and Slack webhook URL to enable alert notifications.
    - Enhanced the consumer script to support alerting functionality for improved monitoring and response capabilities.

--- consumer.sh
+++ consumer.sh
@@ -11,6 +11,14 @@ export FIRE_TOPIC=fires
 # HBase configuration
 export HBASE_REST_URL=http://hadoop-master:8080
 
+# Alert configuration
+export SMTP_SERVER=smtp.gmail.com
+export SMTP_PORT=587
+export ALERT_EMAIL=flowshield77@gmail.com
+export ALERT_EMAIL_PASSWORD=12345678A@
+export ALERT_RECIPIENT=testing7102023@gmail.com
+export ALERT_WEBHOOK_URL=https://hooks.slack.com/services/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
+
 # Submit Spark job
 spark-submit \
   --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0 \
